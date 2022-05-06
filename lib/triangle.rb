052b35bf4e363d3c7c5e20ca41bb4cea032cd337
CharlieRojas <crc_185@yahoo.com>
2022-05-06 01:36:14 -0500

    commit tree 5d938171e875281b1acd7afb6b1a5391ab3b4d53

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize(l1,l2,l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  def kind
+    if self.l1 && self.l2 && self.l3 == 0
+      raise TriangleError
+    elsif self.l1 + self.l2 <= self.l3 || self.l1 + self.l3 <= self.l2 || self.l3 + self.l2 <= self.l1
+      raise TriangleError
+    elsif self.l1 == self.l2 && self.l1 == self.l3
+      :equilateral
+    elsif self.l1 == self.l2 || self.l1 == self.l3 || self.l2 == self.l3 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle Error!"
+    end
+  end
 end
